24154151fc591aebe705c7378bf510c5dffd6ef5
jamesossam <JOssam@scu.edu>
2021-01-25 17:48:15 -0500

    Reworked into functional component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,21 @@
 import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import Home from './Home';
 import Callback from './Callback';
 
-/*
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
- */
-
-class App extends Component {
-
-  render() {
     return (
         <Router>
-          <Switch>
-            <Route exact path="/callback" component={Callback}/>
-            <Route exact path="/" component={Home}/>
-          </Switch>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/callback" component={Callback}/>
+            </Switch>
         </Router>
     );
-  }
 }
+
 
 export default App;
